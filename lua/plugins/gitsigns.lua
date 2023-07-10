b4aeb9d407f59b393a6d8885163472834650c59a
zuoyonghui <zuoyonghui@bytedance.com>
2023-07-10 20:14:39 +0800

    update readme.md

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,7 +10,7 @@ return {
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
       },
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+      signcolumn = false,  -- Toggle with `:Gitsigns toggle_signs`
       numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
       linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
