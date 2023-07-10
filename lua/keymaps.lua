3acc7a53a193f8a99726e971e2211d32e0dd4183
zuoyonghui <zuoyonghui@bytedance.com>
2023-07-10 19:59:00 +0800

    update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -93,13 +93,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-local ls = require('luasnip')
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
-
-vim.keymap.set({"i", "s"}, "<C-E>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
-end, {silent = true})
+local ls = require("luasnip")
+vim.keymap.set({"s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
