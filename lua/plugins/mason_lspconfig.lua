5697c741af78a7b7a5f2b279bc00950d00cc7378
zuoyonghui <zzhattzzh@126.com>
2024-12-27 03:18:32 +0800

    Update mason_lspconfig.lua

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -2,7 +2,7 @@ return {
   "williamboman/mason-lspconfig.nvim",
   config = function()
     require("mason-lspconfig").setup {
-      ensure_installed = { "pyright", "pylsp", "tsserver", "lua_ls" },
+      ensure_installed = { "pyright", "pylsp", "lua_ls" },
     }
   end
 }
